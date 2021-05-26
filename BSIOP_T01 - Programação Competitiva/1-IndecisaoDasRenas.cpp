91519e44b850343623b40040f783cebbf4c8eaa6
Eduardo <pedroserep@gmail.com>
2021-05-25 22:21:35 -0400

    Add 1-Indecisao das Renas.cpp

---
+++ BSIOP_T01 - Programação Competitiva/1-IndecisaoDasRenas.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string renas[] = {"Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen", "Rudolph"};
+
+int main(){
+    int sum = 0, val;
+    for(int i=0;i<9;i++){
+        scanf("%d",&val);
+        sum+=val;
+    }
+    printf("%s\n",renas[(sum-1)%9].data());
+}
