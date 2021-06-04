989667e45c301fd4aadf261ee74d80114a1b041f
Eduardo <pedroserep@gmail.com>
2021-06-04 15:31:25 -0400

    Add 10-Backup

---
+++ BSIOP_T01 - Programação Competitiva/Aula 4 - Gulosos/10-Backup.cpp
@@ -0,0 +1,10 @@
+//Link da questao
+//https://neps.academy/br/exercise/1360
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    int n; scanf("%d",&n);
+    printf("%d\n",__builtin_popcount(n)); //kkkk
+}
