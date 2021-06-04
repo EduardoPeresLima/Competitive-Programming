7c7219c818a69eaa0c793f1cbb5a1ae54a59f493
Eduardo <pedroserep@gmail.com>
2021-06-04 15:25:46 -0400

    Add 07-Dentista

---
+++ BSIOP_T01 - Programação Competitiva/Aula 4 - Gulosos/07-Dentista.cpp
@@ -0,0 +1,26 @@
+//Link da questao
+//https://neps.academy/br/exercise/248
+#include<bits/stdc++.h>
+using namespace std;
+
+bool cmp(pair<int,int> a, pair<int,int> b){
+    if(a.second != b.second) return a.second < b.second;
+    return a.first < b.first;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    int n; cin >> n;
+    vector<pair<int,int>> v(n);
+    for(int i=0;i<n;i++) cin >> v[i].first >> v[i].second;
+    sort(v.begin(),v.end(),cmp);
+
+    int fim = -10, ans = 0;
+    for(int i=0;i<n;i++){
+        if(v[i].first >= fim){
+            ans++;
+            fim = v[i].second;
+        }
+    }
+    cout << ans << endl;
+}
