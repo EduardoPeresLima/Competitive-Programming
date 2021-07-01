8a48daa7e49630a0f8d3bc38d6f35a2124d8cf36
Eduardo <pedroserep@gmail.com>
2021-07-01 14:32:19 -0400

    16-Floresta.cpp

--- BSIOP_T01 - Programação Competitiva/Aula 5 - Busca Binaria/16-Floresta.cpp
+++ BSIOP_T01 - Programação Competitiva/Aula 5 - Busca Binaria/16-Floresta.cpp
@@ -8,7 +8,7 @@ int main(){
     int fim = sqrt(n);
     set<pair<int,int>> ans;
     for(int i=1;i<=fim;i++){
-        if((n-1-i)%(2*i + 1) == 0){
+        if((n-1-i)%(2*i + 1)==0){
             int x = i, y = (n-1-i)/(2*i + 1);
             ans.emplace(min(x,y),max(x,y));
         }
