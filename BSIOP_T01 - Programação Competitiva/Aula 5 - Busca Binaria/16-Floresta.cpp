edd14df8ea5feb1daf2bbcf19a98f4b002b60156
Eduardo <pedroserep@gmail.com>
2021-07-01 14:31:54 -0400

    16

---
+++ BSIOP_T01 - Programação Competitiva/Aula 5 - Busca Binaria/16-Floresta.cpp
@@ -0,0 +1,17 @@
+//Link da questao
+//https://neps.academy/br/exercise/548
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; scanf("%d",&n);
+    int fim = sqrt(n);
+    set<pair<int,int>> ans;
+    for(int i=1;i<=fim;i++){
+        if((n-1-i)%(2*i + 1) == 0){
+            int x = i, y = (n-1-i)/(2*i + 1);
+            ans.emplace(min(x,y),max(x,y));
+        }
+    }
+    printf("%d\n",(int)ans.size());
+}
