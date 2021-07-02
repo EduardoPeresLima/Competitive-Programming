71c11bda147c70b74305ea2f2be7a22ffd6b05a5
Edudu <epl.eng18@uea.edu.br>
2021-07-02 08:32:24 -0400

    19-EHOMAMACO.cpp

---
+++ BSIOP_T01 - Programação Competitiva/Aula 5 - Busca Binaria/19-EHOMAMACO.cpp
@@ -0,0 +1,34 @@
+//Link da questao
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+bool f(int n, vector<int> &v, int k){
+    int last = 0, delta;
+    for(int val : v){
+        delta = val - last;
+        if(delta > k)return false;
+        if(delta == k)k--;
+        last = val;
+    }
+    return true;
+}
+
+int bb(int n, vector<int> &v){
+    int lo = 1, hi = 1e9, mid;
+    while(lo < hi){
+        mid = lo + ((hi-lo)>>1);
+        if(f(n,v,mid))hi = mid;
+        else lo = mid+1;
+    }
+    return lo;
+}
+
+int main(){
+    int t; scanf("%d",&t);
+    for(int caso = 1;caso <=t;caso++){
+        int n; scanf("%d",&n);
+        vector<int> v(n); for(int &x : v)scanf("%d",&x);
+        printf("Case %d: %d\n",caso, bb(n,v));
+    }
+}
