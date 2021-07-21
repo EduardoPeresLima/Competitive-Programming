af2f5d0d9909ffe8400c43d76641c0b96a57515a
Eduardo <pedroserep@gmail.com>
2021-07-20 23:54:01 -0400

    03-RemovingDigits.cpp

--- BSIOP_T01 - Programação Competitiva/Aula 9 - IntroducaoPD/03-RemovingDigits.cpp
+++ BSIOP_T01 - Programação Competitiva/Aula 9 - IntroducaoPD/03-RemovingDigits.cpp
@@ -9,7 +9,9 @@ int main(){
     ios_base::sync_with_stdio(0);
     int n; scanf("%d",&n);
     vector<int> dp(n+1, MOD); dp[0] = 0;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=n;i++){//Qual a resposta para o numero i?
+        //Olho pra resposta de i - "um dos seus digitos"
+        //Minha resposta = menor deles + 1 (+1 pq eu removo um digito pra poder chegar ao [i - "digito escolhido"])
         int x = i;
         while(x){
             dp[i] = min(dp[i],dp[i-x%10]+1);
