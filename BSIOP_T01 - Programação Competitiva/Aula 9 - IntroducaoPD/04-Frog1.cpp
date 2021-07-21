6ffae00f2d4b43c46a35f4f0460e6a6500ab23ae
Eduardo <pedroserep@gmail.com>
2021-07-20 23:01:16 -0400

    04-Frog1.cpp

---
+++ BSIOP_T01 - Programação Competitiva/Aula 9 - IntroducaoPD/04-Frog1.cpp
@@ -0,0 +1,19 @@
+//Link da questao
+//https://atcoder.jp/contests/dp/tasks/dp_a
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long       ll;
+const int MOD = ((int)1e9+7);
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    int n; scanf("%d",&n);
+    vector<int> v(n); for(int &x : v)scanf("%d",&x);
+    vector<int> dp(n);
+    dp[0] = 0;
+    dp[1] = abs(v[1]-v[0]);
+    for(int i=2;i<n;i++){
+        dp[i] = min(dp[i-1] + abs(v[i]-v[i-1]), dp[i-2] + abs(v[i]-v[i-2]));
+    }
+    printf("%d\n",dp[n-1]);
+}
