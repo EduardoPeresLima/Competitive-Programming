8fcba5a0e4cce932b588479f089be17ebb3c80c0
Eduardo <pedroserep@gmail.com>
2021-07-26 15:41:38 -0400

    06-BookExchange.cpp

--- BSIOP_T01 - Programação Competitiva/Aula 7 - DSU/06-BookExchange.cpp
+++ BSIOP_T01 - Programação Competitiva/Aula 7 - DSU/06-BookExchange.cpp
@@ -8,7 +8,7 @@ const int MOD = ((int)1e9+7);
 typedef long long       ll;
 
 int n, m;
-vector<int> pai;
+vector<int> pai, tam;
 int id(int u){
     return pai[u] = u == pai[u]?u:id(pai[u]);
 }
@@ -16,9 +16,22 @@ void join(int u, int v){
     u = id(u), v = id(v);
     if(u==v)return;
     pai[u] = v;
+    tam[v] += tam[u];
+}
+
+void solve(){
+    int n; scanf("%d",&n);
+    pai.assign(n,0); iota(pai.begin(),pai.end(),0);
+    tam.assign(n,1);
+    for(int a,i=0;i<n;i++){
+        scanf("%d",&a);a--;
+        join(i,a);
+    }
+    for(int i=0;i<n;i++)printf("%d ",tam[id(i)]);printf("\n");
 }
 
 int main(){
     ios_base::sync_with_stdio(0);
-    
+    int t; scanf("%d",&t);
+    while(t--)solve();
 }
