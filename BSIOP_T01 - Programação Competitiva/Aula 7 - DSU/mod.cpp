e6fa7a1980925eb2c4c434e6f822454ecdc8b783
Eduardo <pedroserep@gmail.com>
2021-07-26 15:02:34 -0400

    01-FamiliaDeTroia.cpp

---
+++ BSIOP_T01 - Programação Competitiva/Aula 7 - DSU/mod.cpp
@@ -0,0 +1,11 @@
+//Link da questao
+//
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long       ll;
+const int MOD = ((int)1e9+7);
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    
+}
