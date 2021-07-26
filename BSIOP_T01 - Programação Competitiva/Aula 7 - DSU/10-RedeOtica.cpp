94bdd1104e6ff59d2031f43d10ff3051610e1a44
Eduardo <pedroserep@gmail.com>
2021-07-26 17:39:40 -0400

    10-RedeOtica.cpp

---
+++ BSIOP_T01 - Programação Competitiva/Aula 7 - DSU/10-RedeOtica.cpp
@@ -0,0 +1,49 @@
+//Link da questao
+//https://neps.academy/br/exercise/302
+#include<iostream>
+#include<vector>
+#include<numeric> //iota
+#include<algorithm>
+#include<tuple>
+using namespace std;
+typedef vector<vector<pair<int,int>>> graph;
+const int MOD = ((int)1e9+7);
+typedef long long       ll;
+
+int n, m;
+vector<int> pai;
+int id(int u){
+    return pai[u] = u == pai[u]?u:id(pai[u]);
+}
+void join(int u, int v){
+    u = id(u), v = id(v);
+    if(u==v)return;
+    pai[u] = v;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    int teste = 1;
+    while(scanf("%d",&n) && n){
+        scanf("%d",&m);
+        printf("Teste %d\n",teste++);
+        pai.assign(n+1,0); iota(pai.begin(),pai.end(),0);
+        vector<tuple<int,int,int>> edges;
+        int a,b,dist;
+        for(int i=0;i<m;i++){
+            scanf("%d%d%d",&a,&b,&dist);
+            edges.emplace_back(dist,a,b);
+        }
+        sort(edges.begin(),edges.end());
+        int ans = 0;
+        for(auto x : edges){
+            tie(dist,a,b) = x;
+            if(id(a) == id(b))continue;
+            join(a,b);
+            ans += dist;
+            if(a>b)swap(a,b);
+            printf("%d %d\n",a,b);
+        }
+        printf("\n");
+    }
+}
